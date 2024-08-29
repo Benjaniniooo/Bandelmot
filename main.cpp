df747ad33380e723ef7f96e75b34e27f72de3692
Benjaniniooo <39649250+Benjaniniooo@users.noreply.github.com>
2024-08-29 16:47:33 +0200

    first working OpenGL example

--- main.cpp
+++ main.cpp
@@ -9,8 +9,30 @@ unsigned int WINDOW_HEIGHT = 600;
 float lastTime = 0.f;
 unsigned int framecounter = 0;
 
-void resize_callback(GLFWwindow* window, int width, int height)
-{
+float vertices[] = {
+    -0.5f, -0.5f, 0.0f,
+    0.5f, -0.5f, 0.0f,
+    0.0f, 0.5f, 0.0f
+};
+
+const char *vertexShaderGLSL = 
+"#version 460 core\n"
+"layout (location = 0) in vec3 pos;\n"
+"void main()\n"
+"{\n"
+"gl_Position = vec4(pos, 1.0);\n"
+"}\n";
+
+const char *fragmentShaderGLSL = 
+"#version 460 core\n"
+"out vec4 color;\n"
+"void main()\n"
+"{\n"
+"color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\n";
+
+
+void resize_callback(GLFWwindow* window, int width, int height){
     WINDOW_WIDTH = width;
     WINDOW_HEIGHT = height;
     
@@ -32,8 +54,7 @@ void error_callback(int error, const char* description){
     printf("[GLFW ERROR %d]: %s\n", error, description);
 }
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]){
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
@@ -60,6 +81,54 @@ int main(int argc, char const *argv[])
         return -1;
     }
 
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderGLSL, NULL);
+    glCompileShader(vertexShader);
+    
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if(!success){
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION: " << infoLog << std::endl;
+    }
+
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderGLSL, NULL);
+    glCompileShader(fragmentShader);
+    
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if(!success){
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION: " << infoLog << std::endl;
+    }
+
+    unsigned int shaderProg = glCreateProgram();
+    glAttachShader(shaderProg, vertexShader);
+    glAttachShader(shaderProg, fragmentShader);
+    glLinkProgram(shaderProg);
+
+    glGetProgramiv(shaderProg, GL_LINK_STATUS, &success);
+    if(!success){
+        glGetProgramInfoLog(shaderProg, 512, NULL, infoLog);
+        std::cout << "ERROR::PROGRAM::PROGRAM::LINKING: " << infoLog << std::endl;
+    }
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+    
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*) 0);
+    glEnableVertexAttribArray(0);
+
     while(!glfwWindowShouldClose(window)){
         if(framecounter < 100){
             framecounter++;
@@ -69,6 +138,12 @@ int main(int argc, char const *argv[])
             std::cout << "FPS: " << 100.f / (glfwGetTime() - lastTime) << std::endl;
             lastTime = glfwGetTime();
         }
+
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProg);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
